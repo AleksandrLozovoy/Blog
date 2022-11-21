14db2897cd78044f1231caecd4a608e2d9420891
AleksandrLozovoy <lozovoialeksandr2019@gmail.com>
2022-11-21 11:35:02 +0300

    work with gulp #3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const { src, dest } = require("gulp");
+const { src, dest, watch, series, paralel, parallel } = require("gulp");
+const browserSync = require("browser-sync").create();
 
 // Плагины
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
 const fileInclude = require("gulp-file-include");
 const htmlmin = require("gulp-htmlmin");
 const size = require("gulp-size");
@@ -8,6 +11,11 @@ const size = require("gulp-size");
 // обработка HTML
 const html = () => {
   return src("./src/html/*.html")
+    .pipe(
+      plumber({
+        errorHandler: notify.onError(),
+      })
+    )
     .pipe(fileInclude())
     .pipe(size({ title: "До сжатия" }))
     .pipe(
@@ -16,8 +24,27 @@ const html = () => {
       })
     )
     .pipe(size({ title: "После сжатия" }))
-    .pipe(dest("./public"));
+    .pipe(dest("./public"))
+    .pipe(browserSync.stream());
+};
+
+// Cервер
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./public",
+    },
+  });
+};
+
+// Наблюдение
+const watcher = () => {
+  watch("./src/html/**/*.html", html);
 };
 
 // Задачи
 exports.html = html;
+exports.watch = watcher;
+
+// сборка
+exports.dev = series(html, parallel(watcher, server));
