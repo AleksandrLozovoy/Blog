7d5f287bba87e6c18904fc087531beb5da903604
AleksandrLozovoy <lozovoialeksandr2019@gmail.com>
2022-12-24 22:19:51 +0300

    work with js #2

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,11 @@
-console.log("kfjdkfdk");
+const modalBtn = document.querySelectorAll("[data-modal]");
+/* querySelectorAll собирает в массив элементы по указанному селектору*/
+
+modalBtn.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    let $this = event.currentTarget;
+    let modalId = $this.getAttribute("data-modal");
+    let modal = document.getElementById(modalId);
+    modal.classList.add("show");
+  });
+});
